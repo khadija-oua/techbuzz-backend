48097a449f0a1ce1ef2324d7dc95f01a80736b96
asmaSabr <asmasabrri@gmail.com>
2026-04-16 03:48:28 +0100

    refactor(core): update db config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 async function connectDB() {
+  try {
   await mongoose.connect(process.env.MONGO_URI);
   console.log('[MongoDB] Connecté');
+ }catch (err) {
+    console.error('[DB] Erreur de connexion:', err.message);
+    process.exit(1);
+  }
 }
 
 module.exports = connectDB;
